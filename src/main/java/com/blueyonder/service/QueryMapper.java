6c060b09223b92e75dcaa159a779500c8a50fbbf
Nikith KB <nikithkb2002@gmail.com>
2024-02-06 16:03:55 +0530

    Login page assignment

--- src/main/java/com/blueyonder/service/QueryMapper.java
+++ src/main/java/com/blueyonder/service/QueryMapper.java
@@ -2,5 +2,5 @@ package com.blueyonder.service;
 
 public interface QueryMapper {
     public static final String CHECK_LOGIN = "select * from userdata where username=? and password = ?";
-    public static final String NON_ADMIN = "select * from userdata where admin = false";
+    public static final String NON_ADMIN = "select * from userdata where admin = false order by username";
 }
